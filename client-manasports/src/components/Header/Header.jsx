ba4621da7b69bca089087c0648ac8effaaacb717
juanlumoreno <juanlu.calderon@gmail.com>
2020-03-11 00:08:44 +0100

    login modal updated

--- client-manasports/src/components/Header/Header.jsx
+++ client-manasports/src/components/Header/Header.jsx
@@ -3,25 +3,195 @@ import React, { useState } from "react";
 import { connect } from "react-redux";
 import ModalLogin from "./LoginModal";
 import ModalRegister from "./RegisterModal";
-import { Navbar, Nav } from 'react-bootstrap';
+import {
+    Modal,
+    Navbar,
+    Nav,
+    Form,
+    FormControl,
+    InputGroup
+  } from "react-bootstrap";
+  import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const Header = props => {
     const { user } = props;
     const [openModalLogin, setOpenModalLogin] = useState(false);
     const [openModalRegister, setOpenModalRegister] = useState(false);
 
+    // ********* Styles - Styled Components - CSSINJS **********
+
+    const HeaderDiv = styled.header`
+      background-color: rgba(0, 0, 0, 0.2);
+      border-bottom: 1px solid #343a40;
+      z-index: 1000;
+    `;
+    const ButtonRegister = styled.button`
+background-color: #ecf0f1;
+font-size: 0.85em;
+font-weight: bolder;
+&:hover {
+  background-color: #bdc3c7;
+}
+`;
+    const SearchResult = styled.div`
+position: absolute;
+width: 93%;
+left: 25px;
+top: 35px;
+background-color: rgba(108, 117, 125, 1);
+font-size: 0.8em;
+`;
+    const SpanUsername = styled.span`
+font-family: "Source Sans Pro", sans-serif;
+font-size: 0.95em;
+&:hover {
+  text-decoration: underline;
+}
+`;
+    const ButtonHeader = styled.button`
+background-color: transparent;
+border: none;
+box-shadow: inset 0 -4px rgba(255, 193, 7, 0.2);
+height: 55px;
+padding: 10px;
+font-size: 0.9em;
+color: white;
+transition: 0.3s;
+&:hover {
+  box-shadow: inset 0 -4px rgba(255, 193, 7, 0.8);
+}
+&:focus {
+  outline: none;
+}
+`;
+    const ImgSearch = styled.img`
+cursor: pointer;
+`;
+
+    const RowHover = styled.div`
+&:hover {
+  color: yellow;
+}
+`;
+
     return (
-        <>
+        <> <HeaderDiv className="w-100">
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-                <Navbar.Brand href="#home">MANASPORT</Navbar.Brand>
+                <Navbar.Brand>
+                    <a href="/">
+                        <img src="/images/logotipo.png" alt="logo" width="230px" />
+                    </a>
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="text-white">{user && user.username}</Nav>
-                    <Nav>
-                        <button className="btn btn-info" onClick={() => setOpenModalLogin(!openModalLogin)}>Login</button>
-                        <button className="btn btn-success" onClick={() => setOpenModalRegister(!openModalRegister)}>REGISTER</button>
-                    </Nav>
-                </Navbar.Collapse>
+                <Navbar.Collapse
+            id="responsive-navbar-nav"
+            className="justify-content-between"
+          >
+            <Form inline style={{ width: "500px" }} className="ml-2">
+              {user ? (
+                <Link to={"/management"} className="border-0">
+                  <ButtonHeader>Tus Ligas</ButtonHeader>{" "}
+                </Link>
+              ) : (
+                  <Link to={"/leagues"} className="border-0">
+                    <ButtonHeader>Info Ligas</ButtonHeader>{" "}
+                  </Link>
+                )}
+              {!user && (
+                <InputGroup size="sm" className="w-75">
+                  {/* <FormControl
+                    className="bg-dark border border-dark text-light ml-sm-4"
+                    autoFocus
+                    value={inputLeagues}
+                    onChange={UpdateInputLeagues}
+                  /> */}
+                  <InputGroup.Append>
+                    <InputGroup.Text className="bg-dark border border-dark text-light">
+                      <ImgSearch
+                        src="/images/other/search.png"
+                        width="14"
+                        alt=""
+                      />
+                    </InputGroup.Text>
+                  </InputGroup.Append>
+                  {/* {inputLeagues !== "" && (
+                    <SearchResult className="text-light rounded">
+                      {props.allleagues
+                        .filter(l =>
+                          l.name
+                            .toLowerCase()
+                            .includes(inputLeagues.toLowerCase())
+                        )
+                        .slice(0, 5)
+                        .map(l => (
+                          <RowHover className="row p-2" key={l.TournamentId} style={{ cursor: 'pointer' }} onClick={() => history.push("/management/leagueDetails/" + l.TournamentId)
+                          }>
+                            <div className="col-8">{l.name}</div>
+                            <div className="col-4 text-center">
+                              {l.NameAdmin}
+                            </div>
+                          </RowHover>
+                        ))}
+                    </SearchResult>
+                  )} */}
+                </InputGroup>
+              )}
+            </Form>
+            {!user && (
+              <Nav className="w-50 justify-content-end mr-3">
+                <a
+                  className="text-light mr-4"
+                  href="/#"
+                  onClick={() => setOpenModalLogin(!openModalLogin)}
+                >
+                  Iniciar sesión
+                </a>
+                <ButtonRegister
+                  className="btn text-dark pt-0 pb-0 pl-3 pr-3 text-uppercase"
+                  onClick={() => setOpenModalRegister(!openModalRegister)}
+                >
+                  Regístrarse
+                </ButtonRegister>
+              </Nav>
+            )}
+            {user && (
+              <Nav className="w-50 justify-content-end mr-4 align-middle">
+                <Link to={"/management/user"}>
+                  <SpanUsername className="text-light align-middle">
+                    {/* {decoded.username} */}
+                  </SpanUsername>
+                </Link>
+                {/* <Link to={"/management/user"}>
+                  {decoded.avatar === null && (
+                    <img
+                      src="/images/profile/no-profile.png"
+                      width="30"
+                      alt=""
+                    />
+                  )}
+                  {decoded.avatar !== null && (
+                    <img
+                      src={currentUser ? currentUser.avatar : undefined}
+                      width="30"
+                      alt=""
+                      className="ml-3"
+                    />
+                  )}
+                </Link> */}
+                <a href="/#">
+                  <img
+                    src="/images/other/logout.png"
+                    // onClick={logout}
+                    width="20"
+                    alt=""
+                    title="Cerrar sesión"
+                    className="ml-3"
+                  />
+                </a>
+              </Nav>
+            )}
+          </Navbar.Collapse>
             </Navbar>
 
             <ModalRegister
@@ -32,6 +202,7 @@ const Header = props => {
                 show={openModalLogin}
                 onHide={() => setOpenModalLogin(false)}
             />
+        </HeaderDiv>
         </>
     );
 };
--- client-manasports/src/components/Header/LoginModal.jsx
+++ client-manasports/src/components/Header/LoginModal.jsx
@@ -1,9 +1,17 @@
 // DEPENDENCES
 import React, { useState } from "react";
-import Modal from 'react-bootstrap/Modal';
+// import Modal from 'react-bootstrap/Modal';
+import { InputGroup, FormControl, Modal } from "react-bootstrap";
 import { connect } from 'react-redux';
 import { loginAction } from "../../redux/actions/userActions";
+import styled from "styled-components";
+import './header.css';
 // import md5 from "md5";
+
+// ********* Styles - Styled Components - CSSINJS **********
+const Col = styled.div`
+  font-size: 0.85em;
+`
 
 const ModalLogin = (props) => {
 
@@ -30,36 +38,91 @@ const ModalLogin = (props) => {
     };
 
     return (
-        <Modal
-            {...props}
-            size="md"
-            aria-labelledby="contained-modal-title-vcenter"
-            centered
-        >
-            <Modal.Header closeButton>
-                <Modal.Title id="contained-modal-title-vcenter">
-                    LOGIN
-          </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <div className="row">
-                    <div className="col">
-                        Email
-                            <input type="text" name="email" value={user.email} onChange={handleChange} />
+        <>
+            <Modal
+                {...props}
+                size="md"
+                aria-labelledby="contained-modal-title-vcenter"
+                className="loginModalHeader"
+                centered
+            >
+                <Modal.Header className="px-2" closeButton>
+                    <Modal.Title id="contained-modal-title-vcenter">
+                        <h5 className="modal-title pl-2 text-white" id="exampleModalCenterTitle">
+                            Iniciar en ManaSport
+                             </h5>
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <div className="row">
+                        <div className="col">
+                            <InputGroup className="mb-3 rounded">
+                                <InputGroup.Prepend>
+                                    <InputGroup.Text id="basic-addon1" className="pt-0 pb-0">
+                                        <img src="/images/form/email.png" width="15" alt="" />
+                                    </InputGroup.Text>
+                                </InputGroup.Prepend>
+                                <FormControl
+                                    type="text"
+                                    className="form-control pt-0 pb-0 pl-2 pr-2 mt-0"
+                                    style={{ color: 'black' }}
+                                    name="email"
+                                    id="email"
+                                    placeholder="Email"
+                                    value={user.email}
+                                    onChange={handleChange}
+                                />
+                            </InputGroup>
+                        </div>
                     </div>
-                </div>
-                <div className="row">
-                    <div className="col">
-                        Password
-                            <input type="text" name="password" value={user.password} onChange={handleChange} />
+                    <div className="row">
+                        <div className="col">
+                            <InputGroup className="mb-3">
+                                <InputGroup.Prepend>
+                                    <InputGroup.Text id="basic-addon1" className="pt-0 pb-0">
+                                        <img src="/images/form/lock.png" width="15" alt="" />
+                                    </InputGroup.Text>
+                                </InputGroup.Prepend>
+                                <FormControl
+                                    type="password"
+                                    className="form-control pt-0 pb-0 pl-2 pr-2 mt-0"
+                                    placeholder="Contraseña"
+                                    name="password"
+                                    id="myInput"
+                                    value={user.password}
+                                    onChange={handleChange}
+                                />
+                                <InputGroup.Append style={{ cursor: 'pointer' }}>
+                                    <InputGroup.Text>
+                                        <img src="/images/form/eye.png" width="20" alt="" />
+                                    </InputGroup.Text>
+                                </InputGroup.Append>
+                            </InputGroup>
+                        </div>
                     </div>
-                </div>
-                <button onClick={onLogin} type="submit">LOGIN</button>
-            </Modal.Body>
-            <Modal.Footer>
-                <button>Close</button>
-            </Modal.Footer>
-        </Modal>
+                </Modal.Body>
+                <Modal.Footer className="p-0">
+                    <div className="container-fluid">
+                        <div className="row">
+                            <div className="col">
+                                <button
+                                    type="button"
+                                    className="btn btn-warning w-100 font-weight-bold text-uppercase"
+                                    onClick={onLogin}
+                                >
+                                    Iniciar sesión
+                                </button>
+                            </div>
+                        </div>
+                        <div className="row mt-3 mb-3">
+                            <Col className="col text-center text-white">
+                                ¿La primera vez? <a href="/#" className="text-warning text-decoration-none" >Registrate </a>
+                            </Col>
+                        </div>
+                    </div>
+                </Modal.Footer>
+            </Modal>
+        </>
     );
 }
 
